f0140b8100062dbe2d39c2cdf6710dec5dbf3b55
Jefferson Mira <jefferson.mira@mastercard.com>
2017-04-19 07:32:04 -0500

    first commit

---
+++ src/main/java/com/example/jeff/test/GradleTestingApplication.java
@@ -0,0 +1,12 @@
+package com.example.jeff.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTestingApplication.class, args);
+	}
+}
